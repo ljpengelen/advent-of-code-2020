a98ec80616c8eea0add9ae18cee148b609f0bc4d
Luc Engelen <luc@kabisa.nl>
2020-12-01 21:22:12 +0100

    Add transducer-based solution

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Install Clojure by executing `brew install clojure`.
 
 * Execute `clj -X day1/run` to run the program for day 1 with the default tuple size of 2.
 * Execute `clj -X day1/run '[:size]' 3` to run the same program with a tuple size of 3.
+* There's also a variant that uses a transducer with the same interface: `clj -X day1-transducer/run`.
+
+The variant using a transducer could be more efficient in practise, because it doesn't create intermediate sequences, but both variants perform the same in this scenario.
---
+++ src/day1_transducer.clj
@@ -0,0 +1,32 @@
+(ns day1-transducer
+  (:require
+    [clojure.java.io :refer [resource]]
+    [clojure.math.combinatorics :refer [combinations]]
+    [clojure.string :refer [split-lines]]))
+
+(defn to-integers
+  [strings]
+  (map #(Integer/parseInt %) strings))
+
+(defn twenty-twenty
+  [tuple]
+  (= 2020 (apply + tuple)))
+
+(defn mult
+  [tuple]
+  (apply * tuple))
+
+(def xf (comp
+          (filter twenty-twenty)
+          (map mult)))
+
+(defn run
+  [args]
+  (let [size (get args :size 2)
+        tuples (-> "resources/day1.txt"
+                  (slurp)
+                  (split-lines)
+                  (to-integers)
+                  (combinations size))
+        result (transduce xf conj tuples)]
+    (println result)))
